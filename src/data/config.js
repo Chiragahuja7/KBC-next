a3045d35b1cc3c439a8eecfa4f1ce5188e2e6426
zoro <boxldevzoro@gmail.com>
2026-03-13 17:40:44 +0530

    Changed website whole theme 2

---
+++ src/data/config.js
@@ -0,0 +1,7 @@
+export const themeConfig = {
+    primary: "#212e58ff", // Professional Blue (Tailwind blue-800) "#22377dff",
+    primaryHover: "#1e3a8a", // Darker Blue (Tailwind blue-900)
+    primaryLight: "#dbeafe", // Light Blue (Tailwind blue-100)
+    primaryDark: "#0f172a", // Very Dark Blue (Tailwind slate-900)
+    accent: "#f59e0b", // Amber for accents (optional)
+};
